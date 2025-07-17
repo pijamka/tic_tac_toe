0eb6f0c0960a9649b0c21b1b6f6d86f011f5675d
Nikita Gusak <misfitsguk@gmail.com>
2025-07-17 18:05:42 +0500

    complete ex

---
+++ gameparts/exceptions.py
@@ -0,0 +1,18 @@
+class FieldIndexError(IndexError):
+    """Выбрасывается, если выбрано значение вне поля."""
+
+    def __init__(
+            self,
+            # Текст по умолчанию.
+            message='Введено значение за границами игрового поля!'
+        ):
+        super().__init__(message)
+
+
+# Вот оно - новое исключение, унаследованное от базового класса Exception.
+class CellOccupiedError(Exception):
+    def __init__(
+            self,
+            message='Попытка изменить занятую ячейку'
+        ):
+        super().__init__(message)
--- gameparts/parts.py
+++ gameparts/parts.py
@@ -1,8 +1,14 @@
-# parts.py
+# gameparts/parts.py
 
 class Board:
+    """Класс, который описывает игровое поле."""
+
+    field_size = 3
+
     def __init__(self):
-        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+        self.board = [
+            [' ' for _ in range(self.field_size)] for _ in range(self.field_size)
+        ]
 
     def make_move(self, row, col, player):
         self.board[row][col] = player
@@ -11,3 +17,41 @@ class Board:
         for row in self.board:
             print('|'.join(row))
             print('-' * 5)
+    
+    def is_board_full(self):
+        # Цикл проходится по всем столбцам игрового поля.
+        for i in range(self.field_size):
+            # А потом по всем строчкам.
+            for j in range(self.field_size):
+                # Если находит свободную ячейку...
+                if self.board[i][j] == ' ':
+                    # ...игра продолжается.
+                    return False
+        # Иначе - ничья!
+        return True
+    
+    # Этот метод будет определять победу.
+    def check_win(self, player):
+        # Проверка по горизонталям и вертикалям
+        for i in range(self.field_size):
+            if (all([self.board[i][j] == player for j in range(self.field_size)]) or
+                    all([self.board[j][i] == player for j in range(self.field_size)])):
+                return True
+    
+        # Проверка по диагоналям
+        if (all([self.board[i][i] == player for i in range(self.field_size)]) or
+                all([self.board[i][self.field_size - 1 - i] == player for i in range(self.field_size)])):
+            return True
+    
+        return False
+    
+    def save_results(self, current_player):
+        file = open('result.txt', 'a', encoding='utf-8')
+        file.write(current_player + '\n')
+        file.close()
+
+    def __str__(self):
+        return (
+            'Объект игрового поля размером '
+            f'{self.field_size}x{self.field_size}'
+        )
